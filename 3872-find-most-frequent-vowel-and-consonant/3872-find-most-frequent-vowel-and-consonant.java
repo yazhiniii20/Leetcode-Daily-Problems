36672cd9b670fc9625f4978ff9a4e9e46fd46c0d
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2025-09-13 14:13:14 +0530

    Time: 1 ms (100%), Space: 42.9 MB (31.34%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxFreqSum(String s) {
        int n = s.length();
-       HashMap<Character,Integer> map = new HashMap<>();
+       int[] alpha = new int[26];
        for(char c : s.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
+         alpha[c-'a']++;
        }
-       int vowelmax = 0;
-       int consonantmax = 0;
-       for(char c : map.keySet()){
-        if(isVowel(c)){
-            vowelmax = Math.max(vowelmax,map.get(c));
-        }else{
-            consonantmax = Math.max(consonantmax,map.get(c));
-        }
+       int vowelmax=0;
+       int consmax=0;
+       for(int i=0;i<n;i++){
+         if(isVowel(s.charAt(i))){
+            vowelmax = Math.max(vowelmax,alpha[s.charAt(i)-'a']);
+         }else{
+            consmax = Math.max(consmax,alpha[s.charAt(i)-'a']);
+         }
        }
-       return vowelmax + consonantmax;
+       return vowelmax+consmax;
     }
     public static boolean isVowel(char c){
         if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
