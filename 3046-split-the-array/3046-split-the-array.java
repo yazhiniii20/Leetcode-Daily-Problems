94a15adfdcfe0cac2acd6ae010073c982c8d3ed8
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2025-11-24 15:25:55 +0530

    Time: 1 ms (95.27%), Space: 44.1 MB (35.97%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.java
+++ 3046-split-the-array/3046-split-the-array.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
         int n = nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i : nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        int max = 0;
+        for(int i=0;i<n;i++){
+            max = Math.max(max,nums[i]);
         }
-        for(int i : map.keySet()){
-            if(map.get(i) > 2){
+        int[] freq = new int[max+1];
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0;i<=max;i++){
+            if(freq[i] > 2){
                 return false;
             }
         }
