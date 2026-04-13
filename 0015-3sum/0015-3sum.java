2f86ade778acff091f685a210dd21e829174cfc3
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-04-13 14:37:10 +0530

    Time: 30 ms (87.69%), Space: 60.2 MB (12.54%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        Set<List<Integer>> st = new HashSet<>();
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i=0;i<n;i++){
-            Set<Integer> hashset = new HashSet<>();
-            for(int j=i+1;j<n;j++){
-               int third = -(nums[i]+nums[j]);
-               if(hashset.contains(third)){
-                List<Integer> arr = new ArrayList<>();
-                arr.add(nums[i]);
-                arr.add(nums[j]);
-                arr.add(third);
-                Collections.sort(arr);
-                st.add(arr);
-               }
-               hashset.add(nums[j]);
+         if(i > 0 && nums[i] == nums[i-1]){
+            continue;
+         }
+         int j = i+1;
+         int k = n-1;
+         while(j < k){
+            int sum = nums[i]+nums[j]+nums[k];
+            if(sum > 0){
+               k--;
             }
+            else if(sum < 0){
+                j++;
+            }else{
+                List<Integer> temp = new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[j]);
+                temp.add(nums[k]);
+                ans.add(temp);
+                j++;
+                k--;
+                while(j<k && nums[j] == nums[j-1]){
+                    j++;
+                }
+                while(j<k && nums[k] == nums[k+1]){
+                    k--;
+                }
+            }
+         }
         }
-        return new ArrayList<>(st);
+        return ans;
     }
 }
