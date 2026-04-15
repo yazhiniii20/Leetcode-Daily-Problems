61ecfc02ba6d1cf90913e8c3471be3c8a33ec624
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-04-15 11:49:35 +0530

    Time: 9 ms (36.67%), Space: 49.4 MB (15.9%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,19 +4,11 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> ans = new ArrayList<>();
         for(int i=0;i<n;i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.isEmpty() && start <= ans.get(ans.size()-1)[1]){
-                continue;
+            if(ans.isEmpty() || ans.get(ans.size()-1)[1] < intervals[i][0]){
+                ans.add(intervals[i]);
+            }else{
+            ans.get(ans.size() - 1)[1] =Math.max(ans.get(ans.size() - 1)[1], intervals[i][1]);
             }
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0] <= end){
-                    end = Math.max(end , intervals[j][1]);
-                }else{
-                    break;
-                }
-            }
-            ans.add(new int[]{start,end});
         }
         return ans.toArray(new int[ans.size()][]);
     }
