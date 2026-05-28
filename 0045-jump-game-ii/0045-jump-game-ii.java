a643f032eb221419e783ddde18a46ec89f961c6f
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-05-28 17:39:18 +0530

    Time: 49 ms (11.03%), Space: 47 MB (92.28%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -2,8 +2,20 @@ class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        dp[0] = 0;
-        return minjumps(0,n,nums,dp);
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[n-1] = 0;
+        for(int i=n-2;i>=0;i--){
+           int jumps = 0;
+           for(int j=1;j<=nums[i];j++){
+            if(i+j < n){
+            jumps = dp[i+j];
+            if(jumps != Integer.MAX_VALUE) jumps = 1+jumps;
+            dp[i] = Math.min(jumps,dp[i]);
+            }
+           }
+        }
+        return dp[0];
+        //return minjumps(0,n,nums,dp);
     }
     static int minjumps(int index,int n,int[] nums,int[] dp){
         if(index >= n-1){
