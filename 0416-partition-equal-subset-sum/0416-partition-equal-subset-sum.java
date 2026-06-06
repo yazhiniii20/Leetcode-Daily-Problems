488b3c728ed4f7386576804544c57288d95eeed4
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-06 13:24:13 +0530

    Time: 50 ms (46.88%), Space: 47.7 MB (49.82%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -5,8 +5,21 @@ class Solution {
         for(int i=0;i<n;i++) totalSum += nums[i];
         if(totalSum%2 != 0) return false;
         int sum = totalSum/2;
-        Boolean[][] dp = new Boolean[n][sum+1];
-        return subsetSumEqualsTarget(n-1,nums,sum,dp);
+        boolean[][] dp = new boolean[n][sum+1];
+        for(int i=0;i<n;i++){
+            dp[i][0] = true;
+        }
+        if(nums[0] <= sum) dp[0][nums[0]] = true;
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                boolean notTake = dp[i-1][target];
+                boolean take = false;
+                if(target >= nums[i])  take = dp[i-1][target-nums[i]];
+                dp[i][target] = (notTake || take);
+            }
+        }
+        return dp[n-1][sum];
+        //return subsetSumEqualsTarget(n-1,nums,sum,dp);
     }
     static boolean subsetSumEqualsTarget(int index,int[] nums,int target,Boolean[][] dp){
         if(target == 0) return true;
