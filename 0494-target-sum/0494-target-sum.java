227eeb91d209ffcc87a229ceb600600fc0b03fd5
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-11 13:29:47 +0530

    Time: 5 ms (78.87%), Space: 44.7 MB (50.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,10 +1,28 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
+    public int findTargetSumWays(int[] arr, int target) {
+        int n = arr.length;
         int totalSum = 0;
-        for(int i=0;i<n;i++) totalSum += nums[i];
+        for(int i=0;i<n;i++) totalSum += arr[i];
         if(totalSum-target < 0 || (totalSum-target)%2 != 0) return 0;
-        return findWays(n-1,nums,(totalSum-target)/2);
+        int sum = (totalSum-target)/2;
+        int[][] dp = new int[n][sum+1];
+        dp[0][0] = 2;
+        if(arr[0] == 0){
+            dp[0][0] = 2;
+        }else{
+            dp[0][0] = 1;
+            if(arr[0] <= sum) dp[0][arr[0]] = 1;
+        }
+        for(int index=1; index<n; index++){
+            for(int t = 0; t <= sum; t++){
+               int notTake = dp[index-1][t];
+               int take = 0;
+               if(t >= arr[index]) take = dp[index-1][t-arr[index]];
+               dp[index][t] = take+notTake;
+            }
+        }
+        return dp[n-1][sum];
+        //return findWays(n-1,nums,(totalSum-target)/2);
     }
     static int findWays(int index,int[] nums,int t){
         if(index == 0){
