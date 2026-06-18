e4deb84b1e596d5fe354f1e7a813748efd8a9766
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-18 11:56:03 +0530

    Time: 19 ms (72.33%), Space: 54.6 MB (29.81%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,9 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        Integer[][] dp = new Integer[m+1][n+1];
-        return countWays(m-1,n-1,s,t,dp);
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
+        for(int j=1;j<=m;j++) dp[0][j] = 0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+            if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+        //return countWays(m-1,n-1,s,t,dp);
     }
     static int countWays(int i,int j,String s,String t,Integer[][] dp){
         if(j < 0) return 1;
