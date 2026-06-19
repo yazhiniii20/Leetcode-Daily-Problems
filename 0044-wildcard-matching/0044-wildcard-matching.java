42ee4f3bcfe17932363743c44b31177ce51c37f3
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-19 15:11:44 +0530

    Time: 56 ms (19.69%), Space: 47.6 MB (41.52%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,23 +2,46 @@ class Solution {
     public boolean isMatch(String s, String p) {
       int n = s.length();
       int m = p.length();
-      Boolean[][] dp = new Boolean[n+1][m+1];
-      return match(n-1,m-1,s,p,dp);   
+
+      boolean[][] dp = new boolean[n+1][m+1];
+
+      dp[0][0] = true;
+      for(int i=1;i<=n;i++) dp[i][0] = false;
+      for(int j=1;j<=m;j++){
+        boolean flag = true;
+        for(int k=1;k<=j;k++){
+          if(p.charAt(k-1) != '*'){
+             flag = false;
+             break;
+          } 
+        } 
+        dp[0][j] = flag;
+      }
+
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+           if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') dp[i][j] = dp[i-1][j-1];
+           else if(p.charAt(j-1) == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+           else dp[i][j] = false;
+        }
+      }
+      return dp[n][m];
+      //return match(n,m,s,p,dp);   
     }
     static boolean match(int i,int j,String s,String p,Boolean[][] dp){
-        if(i < 0 && j < 0) return true;
-        if(j < 0 && i >= 0) return false;
-        if(i < 0 && j >= 0){
-            for(int k=0;k<=j;k++){
-                if(p.charAt(k) != '*') return false;
+        if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false;
+        if(i == 0 && j > 0){
+            for(int k=1;k<=j;k++){
+                if(p.charAt(k-1) != '*') return false;
             }
             return true;
         }
 
         if(dp[i][j] != null) return dp[i][j];
 
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') return dp[i][j] = match(i-1,j-1,s,p,dp);
-        if(p.charAt(j) == '*') return dp[i][j] = match(i-1,j,s,p,dp) || match(i,j-1,s,p,dp);
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') return dp[i][j] = match(i-1,j-1,s,p,dp);
+        if(p.charAt(j-1) == '*') return dp[i][j] = match(i-1,j,s,p,dp) || match(i,j-1,s,p,dp);
         return false;
     }
 }
