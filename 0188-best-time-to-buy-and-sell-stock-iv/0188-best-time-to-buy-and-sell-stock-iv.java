207e97f5fdf43804306a8c3863df866cba27bae8
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-22 17:09:01 +0530

    Time: 2 ms (71.12%), Space: 45.3 MB (45.46%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,8 +1,22 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        Integer[][][] dp = new Integer[n+1][2][k+1];
-        return possibleProfit(0,1,prices,k,dp); 
+        int [][][] dp = new int[n+1][2][k+1];
+        int profit = 0;
+        for(int index = n-1; index >= 0;index --){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int transactions=k;transactions > 0; transactions--){
+                if(buy == 1){
+    profit = Math.max(-prices[index] + dp[index+1][0][transactions],0 + dp[index+1][1][transactions]);
+     }else{
+    profit = Math.max(prices[index] + dp[index+1][1][transactions-1],0 + dp[index+1][0][transactions]);
+     }
+                dp[index][buy][transactions] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+        //return possibleProfit(0,1,prices,k,dp); 
     }
     static int possibleProfit(int index,int buy,int[] prices,int transactions,Integer[][][] dp){
      if(transactions == 0 || index == prices.length) return 0;
