da5e85b48e33d9cc5415c9f2445195854c8ac957
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-06-22 16:47:15 +0530

    Time: 52 ms (42.58%), Space: 118.1 MB (26.15%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,8 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
        int n = prices.length;
-       Integer[][][] dp = new Integer[n+1][2][3];
-       return possibleProfit(0,1,prices,2,dp); 
+       int[][][] dp = new int[n+1][2][3];
+       int profit = 0;
+    //    for(int i=0;i<=n;i++){
+    //       for(int b=0;b<=1;b++){
+    //         dp[i][b][2] = 2;
+    //         if(i == n) dp[i][b][2] = 0;
+    //       }
+    //    }
+       for(int index = n-1;index >= 0;index--){
+         for(int buy=0;buy<=1;buy++){
+            for(int transactions=2;transactions>0;transactions--){
+                 if(buy == 1){
+                profit = Math.max(-prices[index] + dp[index+1][0][transactions], 0 + dp[index+1][1][transactions]);
+             }else{
+                profit = Math.max(prices[index] + dp[index+1][1][transactions-1],
+        0 + dp[index+1][0][transactions]);
+     }
+             dp[index][buy][transactions] = profit;
+            }
+        }
+       }
+       return dp[0][1][2];
+       //return possibleProfit(0,1,prices,2,dp); 
     }
     static int possibleProfit(int index,int buy,int[] prices,int transactions,Integer[][][] dp){
      if(transactions == 0 || index == prices.length) return 0;
