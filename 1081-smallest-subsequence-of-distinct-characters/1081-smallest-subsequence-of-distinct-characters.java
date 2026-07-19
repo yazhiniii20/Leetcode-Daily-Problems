58765c37d7c26aa964848f1d2863a2f4d319e86c
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-07-19 19:54:27 +0530

    Time: 3 ms (71.44%), Space: 42.8 MB (82.17%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] last = new int[26];
+        boolean[] visited = new boolean[26];
+        for(int i=0;i<s.length();i++){
+            last[s.charAt(i)-'a'] = i;
+        }
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(visited[ch-'a']) continue;
+            while(!st.isEmpty() && st.peek() > ch && last[st.peek()-'a'] > i){
+                visited[st.pop()-'a'] = false;
+            }
+            st.push(ch);
+            visited[ch-'a'] = true;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (char ch : st) ans.append(ch);
+        return ans.toString();
+    }
+}
