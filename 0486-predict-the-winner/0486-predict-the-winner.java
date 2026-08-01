00cc5f6783d0a8176243603e6c375ac8089ed813
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-08-01 12:43:38 +0530

    Time: 0 ms (100%), Space: 42.4 MB (94.79%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int ans = f(0,n-1,nums);
+        Integer[][] dp = new Integer[n][n];
+        int ans = f(0,n-1,nums,dp);
         return (ans < 0)? false : true;
     }
-    static int f(int left,int right,int[] nums){
+    static int f(int left,int right,int[] nums,Integer[][] dp){
         if(left == right) return nums[left];
-        int leftPick = nums[left] - f(left+1,right,nums);
-        int rightPick = nums[right] - f(left,right-1,nums);
-        return Math.max(leftPick,rightPick);
+
+        if(dp[left][right] != null) return dp[left][right];
+
+        int leftPick = nums[left] - f(left+1,right,nums,dp);
+        int rightPick = nums[right] - f(left,right-1,nums,dp);
+        
+        return dp[left][right] = Math.max(leftPick,rightPick);
     }
 }
