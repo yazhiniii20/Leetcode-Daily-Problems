2496b299def295e81897e2178b6f111a23dcbc28
Yazhini S <160877024+yazhiniii20@users.noreply.github.com>
2026-08-03 11:03:42 +0530

    Time: 6 ms (92.61%), Space: 85.4 MB (76.7%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,8 +1,19 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        Integer[] dp = new Integer[n+1];
-        int ans = solve(0,n,stoneValue,dp);
+        int[] dp = new int[n+1];
+        dp[n] = 0;
+        for(int index=n-1;index>=0;index--){
+            int ans = Integer.MIN_VALUE;
+            int sum = 0;
+          for(int i=0;i<3;i++){ 
+           if(index+i >= n) break;
+           sum += stoneValue[index+i];
+           ans = Math.max(ans,sum-dp[index+i+1]);
+          }
+          dp[index] = ans;
+        }
+        int ans = dp[0];
         if(ans == 0){
             return "Tie";
         }
